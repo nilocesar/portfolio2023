ec8189d36c92f8c6206409bc6277f1c9ce875f1e
Nilo César <e_maildonilo@yahoo.com.br>
2023-04-30 11:22:26 -0300

    [init] projeto config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,3 @@
-import { Inter } from 'next/font/google';
-import Image from 'next/image';
-
-const inter = Inter({ subsets: ['latin'] });
-
 export default function Home() {
-  return <h1>teste</h1>;
+  return <main></main>;
 }
