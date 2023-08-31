338bbecb00d62a3015e3c1eb57286af6d110ce13
nilo.cesar <nilo.cesar@afferolab.com.br>
2023-08-30 23:12:13 -0300

    ajustes na migalha de pão versao mobile bug

--- src/app/components/CardModel.tsx
+++ src/app/components/CardModel.tsx
@@ -10,15 +10,39 @@ export function CardModel({ it }: Props) {
   // const imgLoading = (image: HTMLImageElement) => {
   //   image.classList.remove('opacity-0');
   // };
+  // console.log(it);
 
   return (
-    <li className="relative h-[10rem] w-full text-black border-b-[0.1rem] divide-slate-700">
-      <button className="opacity-[0.2] hover:opacity-[0.7] cursor-pointer">
+    <li className={`relative h-[10rem] w-full border-b-[0.1rem] animate-fadeIn`}
+    style={
+      {
+        opacity: 0,
+        animationDelay: `${(it+1) * 100}ms`,
+      }
+    }
+    >
+      <video src='./spritesheet.webm' 
+            className={`h-[100%] w-full object-cover absolute animate-fadeOut`} 
+            autoPlay={true} loop={false} muted
+            style={
+                {
+                  animationDelay: `${(it+1) * 200}ms`,
+                }
+              }
+            />
+      <button className={`animate-fadeIn`}
+        style={
+          {
+            opacity: 0,
+            animationDelay: `${(it+1) * 100}ms`,
+          }
+        }
+      > 
         <Image
           src={`https://picsum.photos/600/400`}
           alt=""
           fill
-          className="transition-opacity opacity-1 duration-[2s] object-cover"
+          className={`object-cover opacity-[0.3] hover:opacity-[0.7] cursor-pointer`}
           // onLoadingComplete={(img) => imgLoading(img)}
         />
       </button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,26 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      
+      // that is animation class
+      animation: {
+        fadeIn: 'fadeIn 0.3s ease-in-out forwards',
+        fadeOut: 'fadeOut 0.2s ease-in-out forwards',
+      },
+
+      // that is actual animation
+      keyframes: theme => ({
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 }
+        },
+        fadeOut: {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 }
+        },
+      }),
+    },
   },
   plugins: []
 };
