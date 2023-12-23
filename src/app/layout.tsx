7280debc0131c04b39459b4ad62302f952c2b3e3
nilo.cesar <nilo.cesar@afferolab.com.br>
2023-12-23 17:57:35 -0300

    [FIX] Remove template abount

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+'use client';
+
 import '../styles/globals.scss';
 import '../styles/sidebar.scss';
 
-import type { Metadata } from 'next';
+// import type { Metadata } from 'next';
 import { Jura } from 'next/font/google';
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 
 import { AppWrapper } from 'context';
 
@@ -11,11 +13,11 @@ import { Canvas } from '../components/Canvas';
 import { Sidebar } from '../components/Sidebar';
 import { useLayout } from '../hooks/useLayout';
 
-export const metadata: Metadata = {
-  title: 'Nilo César',
-  description:
-    'Sou um desenvolvedor front-end, empenhado em criar experiências digitais notáveis. Minhas habilidades especializadas em HTML, CSS e JavaScript, aliadas ao domínio dos seus principais frameworks, me capacitam a transformar conceitos criativos em interfaces de usuário envolventes e impactantes.'
-};
+// export const metadata: Metadata = {
+//   title: 'Nilo César',
+//   description:
+//     'Sou um desenvolvedor front-end, empenhado em criar experiências digitais notáveis. Minhas habilidades especializadas em HTML, CSS e JavaScript, aliadas ao domínio dos seus principais frameworks, me capacitam a transformar conceitos criativos em interfaces de usuário envolventes e impactantes.'
+// };
 
 const ralewayFont = Jura({
   subsets: ['latin'],
@@ -28,19 +30,22 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   const randomInteger = (min: number, max: number) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   };
-  const sb_w_randow = randomInteger(17, 35);
+  // const sb_w_randow = randomInteger(17, 35);
+
+  const [stado] = useState(randomInteger(17, 35));
 
   return (
     <html lang="en" className={ralewayFont.className}>
       <body className="bg-white">
-        <Canvas />
-        <div className={`h-screen flex flex-initial flex-wrap ${modelSelect.base}`}>
-          <h1>TesteKKKKK : {sb_w_randow}</h1>
-          <AppWrapper>
+        <AppWrapper>
+          <Canvas />
+          <div className={`h-screen flex flex-initial flex-wrap ${modelSelect.base}`}>
+            <h1>stado : {stado}</h1>
+
             <Sidebar modelSelect={modelSelect.sidebar} />
             {children}
-          </AppWrapper>
-        </div>
+          </div>
+        </AppWrapper>
       </body>
     </html>
   );
